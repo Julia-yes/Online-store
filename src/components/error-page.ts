f5277613c4151ac97bfca10f5b7ad1211227d224
Julia-yes <ec.ho@mail.ru>
2022-12-16 11:40:38 +0400

    feat: add main files and relationship

--- src/components/error-page.ts
+++ src/components/error-page.ts
@@ -1,10 +1,8 @@
-const errorArea = document.querySelector(".main");
-console.log(errorArea)
-
-function renderErrorPage() : void {
+export function renderErrorPage() : void {
+  const errorArea = document.querySelector(".main");
+  console.log(errorArea)
   if (errorArea) {
+    console.log("!!!")
     errorArea.innerHTML = `<div class='error__block'>PAGE NOT FOUND (404)</div>`
   }
 }
-
-renderErrorPage()
--- src/components/main-template.ts
+++ src/components/main-template.ts
@@ -1,14 +1,26 @@
+import {handleLocation} from './routing';
+
 const templateArea = document.querySelector(".store__project");
 
 function renderTemplate() : void {
   if (templateArea) {
     templateArea.innerHTML = `
     <div class = 'wrapper'>
-      <header class = 'header'>Header</header>
-      <main class = 'main'>Main</main>
+      <header class = 'header'>Header
+        <a href='/'>Logo</a>
+        <a href='/cart'>Cart</a>
+      </header>
+      <main class = 'main'>Main
+        <a href='/product'>Product</a>
+      </main>
       <footer class = 'footer'>Footer</footer>
     </div>`
   }
 }
 
-renderTemplate()
+function initProject() : void {
+  renderTemplate();
+  handleLocation();
+}
+
+initProject()
