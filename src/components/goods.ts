8bbf359883615b58d15497f4c27c5a132fd9adb2
Julia-yes <ec.ho@mail.ru>
2022-12-23 13:09:53 +0400

    feat: add string with empty result

--- src/components/goods.ts
+++ src/components/goods.ts
@@ -8,27 +8,33 @@ export function renderGoods() {
   const goodsArea = document.querySelector('.goods__area');
   const goods = products.products;
   let goodsInner = `<div class='goods__wrapper'>`;
-  for (let key in goods) {
-    let good = goods[key];
-    goodsInner += `<div class='goods__card card'>
-      <div class='card__thumbnail'>
-        <img alt='good' class='card__img' src='${good.thumbnail}'>
-        <div class='card__discont'>${good.discountPercentage}&#37;</div>
-      </div>
-      <div class='card__title'>${good.title}</div>
-      <div class='card__price'>
-        <div class='card__price_new'>${good.price}&#36;</div>
-        <div class='card__price_old'>${Math.floor(good.price / (100 - good.discountPercentage) * 100)}&#36;</div>
-      </div>
-      <div class='card__bottom'>
-        <div class='card__rating'>${good.rating}&starf;</div>
-        <div class='card__stock'>Stock &#58; &#32; ${good.stock}</div>
-      </div>
-      <div class='card__buttons'>
-        <button class='card__button card__button_add'>Add to cart</button>
-        <button class='card__button card__button_more' data-goodID = "${good.id}">See more</button>
-      </div>
-    </div>`
+  console.log("!!!", goods.length)
+  if (goods.length == 0) {
+    goodsInner += `<div class='goods__empty'> No products found </div>`
+  }
+  else {
+    for (let key in goods) {
+      let good = goods[key];
+      goodsInner += `<div class='goods__card card'>
+        <div class='card__thumbnail'>
+          <img alt='good' class='card__img' src='${good.thumbnail}'>
+          <div class='card__discont'>${good.discountPercentage}&#37;</div>
+        </div>
+        <div class='card__title'>${good.title}</div>
+        <div class='card__price'>
+          <div class='card__price_new'>${good.price}&#36;</div>
+          <div class='card__price_old'>${Math.floor(good.price / (100 - good.discountPercentage) * 100)}&#36;</div>
+        </div>
+        <div class='card__bottom'>
+          <div class='card__rating'>${good.rating}&starf;</div>
+          <div class='card__stock'>Stock &#58; &#32; ${good.stock}</div>
+        </div>
+        <div class='card__buttons'>
+          <button class='card__button card__button_add'>Add to cart</button>
+          <button class='card__button card__button_more' data-goodID = "${good.id}">See more</button>
+        </div>
+      </div>`
+    }
   }
   if (goodsArea) {
     goodsArea.innerHTML = goodsInner;
